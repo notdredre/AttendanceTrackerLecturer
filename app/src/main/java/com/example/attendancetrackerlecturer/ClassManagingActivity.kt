c1bf3e70b3bddac9089d98f96eeb52500a782938
notdredre <87097301+notdredre@users.noreply.github.com>
2024-10-05 14:42:41 -0400

    Probably implemented WFD manager

--- app/src/main/java/com/example/attendancetrackerlecturer/ClassManagingActivity.kt
+++ app/src/main/java/com/example/attendancetrackerlecturer/ClassManagingActivity.kt
@@ -1,20 +1,85 @@
 package com.example.attendancetrackerlecturer
 
+import android.content.Context
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pGroup
+import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.attendancetrackerlecturer.peerlist.PeerListAdapter
+import com.example.attendancetrackerlecturer.peerlist.PeerListAdapterInterface
+import com.example.attendancetrackerlecturer.wifidirect.WifiDirectInterface
+import com.example.attendancetrackerlecturer.wifidirect.WifiDirectManager
 
-class ClassManagingActivity : AppCompatActivity() {
+class ClassManagingActivity : AppCompatActivity(), WifiDirectInterface, PeerListAdapterInterface {
+    private var wfdAdapterEnabled = false
+    private var wfdHasConnection = false
+    private var hasDevices = false
+    private var peerListAdapter: PeerListAdapter? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_class_managing)
+        setContentView(R.layout.wifi_turned_off)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val manager: WifiP2pManager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+        val channel = manager.initialize(this, mainLooper, null)
+        val wfdManager = WifiDirectManager(manager, channel, this)
+
+        peerListAdapter = PeerListAdapter(this)
+        val rvPeerList: RecyclerView = findViewById(R.id.rvPeerListing)
+        rvPeerList.adapter = peerListAdapter
+        rvPeerList.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onWiFiDirectStateChanged(isEnabled: Boolean) {
+        wfdAdapterEnabled = isEnabled
+        if (!isEnabled) {
+            val toast = Toast.makeText(this, "WiFi adapter is disabled. Please turn it on in order to create groups.", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        updateUI()
+    }
+
+    override fun onPeerListUpdated(deviceList: Collection<WifiP2pDevice>) {
+        hasDevices = deviceList.isNotEmpty()
+        val toast = Toast.makeText(this, "Updated listing of nearby WiFi Direct devices", Toast.LENGTH_SHORT)
+        toast.show()
+        updateUI()
+    }
+
+    override fun onGroupStatusChanged(groupInfo: WifiP2pGroup?) {
+        wfdHasConnection = groupInfo != null
+        val text = if (groupInfo == null){
+            "Group is not formed"
+        } else {
+            "Group has been formed"
+        }
+        val toast = Toast.makeText(this, text , Toast.LENGTH_SHORT)
+        toast.show()
+
+    }
+
+    override fun onDeviceStatusChanged(thisDevice: WifiP2pDevice) {
+        val toast = Toast.makeText(this, "Device parameters have been updated" , Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    private fun updateUI() {
+        if (wfdAdapterEnabled) null
+            //setContentView(r)
     }
 }
---
+++ app/src/main/java/com/example/attendancetrackerlecturer/peerlist/PeerListAdapterInterface.kt
@@ -0,0 +1,7 @@
+package com.example.attendancetrackerlecturer.peerlist
+
+import android.net.wifi.p2p.WifiP2pDevice
+
+interface PeerListAdapterInterface {
+    fun onPeerClicked(peer: WifiP2pDevice)
+}
